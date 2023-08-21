505ae5f42989014f1b85b5db79ea2535354c2ce3
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2023-08-21 15:22:39 +0100

    A function that calculates the Euler's constant value

---
+++ Euler_const.py
@@ -0,0 +1,29 @@
+from decimal import Decimal, getcontext
+
+def calc_euler_const():
+    
+    while True:
+        try:
+            num = int(input('Enter the number of decimal places (1 - 1000) you want the value of e: ' ))
+            if num <= 0 or num > 1000:
+                print('Invalid num. Please enter a number between 1 and 1000')
+            else:
+                break
+        except ValueError:
+            print('Invalid input, please enter a valid number')
+    
+    getcontext().prec = num + 1
+
+    e = Decimal(1)
+    factorial = Decimal(1)
+
+    for n in range(1, num + 1):
+        factorial *= n
+        e += Decimal(1)/factorial
+
+    return str(e)
+
+
+if __name__ == '__main__':
+    euler_const = calc_euler_const()
+    print(euler_const)
