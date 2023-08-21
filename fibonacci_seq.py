160cdc866bb2888e5f51dd53314611a13541c2bb
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2023-08-21 20:08:32 +0100

    Improved the PEP8 rating

--- fibonacci_seq.py
+++ fibonacci_seq.py
@@ -1,13 +1,15 @@
-def fibonacci_sequence(n):
+def fibonacci_sequence(seq_num):
+    """
+    A function that returns the nth number of a fibonacci sequence
+    """
     sequence = [1,1]
 
-    if n == 1:
-            return sequence[0]
+    if seq_num == 1:
+        return sequence[0]
 
-    while n > len(sequence):
+    while seq_num > len(sequence):
         next_num = sequence[-1] + sequence[-2]
         sequence.append(next_num)
-    
     return sequence
 
 
@@ -21,6 +23,5 @@ if __name__ == '__main__':
                 break
         except ValueError:
             print('Invalid Input, please enter a valid number of sequence')
-    
     fib_sequence = fibonacci_sequence(num)
     print(fib_sequence)
